ef9457c53c435dd4b5c61438e29cc68e45b703f7
Joel Uckelman <uckelman@nomic.net>
2015-03-10 20:19:02 +0000

    Made DMNES repo location a parameter.

--- scripts/load_db.sh
+++ scripts/load_db.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash -ex
 
+DMNES=$1
+
 rm -f dmnes.sqlite
 sqlite3 dmnes.sqlite <create.sql
 
-find ../site/dmnes/bib -type f -print0 | xargs -0 ./load_bib.py dmnes.sqlite span.xsl
+find $DMNES/bib -type f -print0 | xargs -0 ./load_bib.py dmnes.sqlite span.xsl
 
-find ../site/dmnes/CNFs -type f -print0 | xargs -0 ./load_cnf.py dmnes.sqlite ../site/dmnes/schemata/cnf.xsd span.xsl
+find $DMNES/CNFs -type f -print0 | xargs -0 ./load_cnf.py dmnes.sqlite $DMNES/schemata/cnf.xsd span.xsl
 
-find ../site/dmnes/VNFs -type f -print0 | xargs -0 ./load_vnf.py dmnes.sqlite ../site/dmnes/schemata/vnf.xsd span.xsl
+find $DMNES/VNFs -type f -print0 | xargs -0 ./load_vnf.py dmnes.sqlite $DMNES/schemata/vnf.xsd span.xsl
