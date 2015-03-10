cbd180e2f8e7945447cc0d4722506f18eebcf60d
Joel Uckelman <uckelman@nomic.net>
2015-03-10 20:27:27 +0000

    Limit finds to files with .xml extension.

--- scripts/load_db.sh
+++ scripts/load_db.sh
@@ -5,8 +5,8 @@ DMNES=$1
 rm -f dmnes.sqlite
 sqlite3 dmnes.sqlite <create.sql
 
-find $DMNES/bib -type f -print0 | xargs -0 ./load_bib.py dmnes.sqlite span.xsl
+find $DMNES/bib -type f -name *.xml -print0 | xargs -0 ./load_bib.py dmnes.sqlite span.xsl
 
-find $DMNES/CNFs -type f -print0 | xargs -0 ./load_cnf.py dmnes.sqlite $DMNES/schemata/cnf.xsd span.xsl
+find $DMNES/CNFs -type f -name *.xml -print0 | xargs -0 ./load_cnf.py dmnes.sqlite $DMNES/schemata/cnf.xsd span.xsl
 
-find $DMNES/VNFs -type f -print0 | xargs -0 ./load_vnf.py dmnes.sqlite $DMNES/schemata/vnf.xsd span.xsl
+find $DMNES/VNFs -type f -name *.xml -print0 | xargs -0 ./load_vnf.py dmnes.sqlite $DMNES/schemata/vnf.xsd span.xsl
