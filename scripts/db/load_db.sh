9f95ee240bc7f203477262687686923b5f95d572
Joel Uckelman <uckelman@nomic.net>
2015-03-26 18:57:36 +0000

    Cleanup.

--- scripts/db/load_db.sh
+++ scripts/db/load_db.sh
@@ -8,7 +8,5 @@ rm -f $DB
 sqlite3 $DB <$HERE/create.sql
 
 $HERE/load_bib.py $DB $HERE/span.xsl $DMNES/bib
-
 $HERE/load_cnf.py $DB $DMNES/schemata/cnf.xsd $HERE/span.xsl $DMNES/CNFs
-
 $HERE/load_vnf.py $DB $DMNES/schemata/vnf.xsd $HERE/span.xsl $DMNES/VNFs
