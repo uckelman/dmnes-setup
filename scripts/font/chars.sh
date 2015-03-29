426033d5efb5c5828357fe4e46b656fc9bd216f9
Joel Uckelman <uckelman@nomic.net>
2015-03-29 23:56:22 +0100

    Print chars to stdout.

--- scripts/font/chars.sh
+++ scripts/font/chars.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 REPO=$1
 SITE=$2
 
-(echo -ne '\n\0' ; (cat $SITE/templates/*.html ; (find $REPO/bib $REPO/CNFs $REPO/VNFs -type f -print0 | xargs -0 -- xsltproc strip.xsl )) | tr -d '\n' | sed -e 's/./&\x00/g') | LC_COLLATE=C sort -zu | tr -d '\0' >char
+(echo -ne '\n\0' ; (cat $SITE/templates/*.html ; (find $REPO/bib $REPO/CNFs $REPO/VNFs -type f -print0 | xargs -0 -- xsltproc strip.xsl )) | tr -d '\n' | sed -e 's/./&\x00/g') | LC_COLLATE=C sort -zu | tr -d '\0'
