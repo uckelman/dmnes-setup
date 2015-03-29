2b4c9184e3880b84690b14e695512d1a71b3ebfd
Joel Uckelman <uckelman@nomic.net>
2015-03-29 23:08:01 +0100

    Let the repo and site dirs be parameters.

--- scripts/font/chars.sh
+++ scripts/font/chars.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-(echo -ne '\n\0' ; (cat ../site/templates/*.html ; (find ../site/dmnes/bib ../site/dmnes/CNFs ../site/dmnes/VNFs -type f -print0 | xargs -0 -- xsltproc strip.xsl )) | tr -d '\n' | sed -e 's/./&\x00/g') | LC_COLLATE=C sort -zu | tr -d '\0' >char
+REPO=$1
+SITE=$2
+
+(echo -ne '\n\0' ; (cat $SITE/templates/*.html ; (find $REPO/bib $REPO/CNFs $REPO/VNFs -type f -print0 | xargs -0 -- xsltproc strip.xsl )) | tr -d '\n' | sed -e 's/./&\x00/g') | LC_COLLATE=C sort -zu | tr -d '\0' >char
