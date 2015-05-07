5b91018445b7785aee4ad564a4b7d3b3a965797e
Joel Uckelman <uckelman@nomic.net>
2015-05-07 21:25:12 +0100

    Typo.

--- scripts/db/live_cnfs_with_pending_vnfs.sh
+++ scripts/db/live_cnfs_with_pending_vnfs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-sqlite3 dmnes.sqlite 'SELECT DISTINCT cnf.nym FROM cnf INNER JOIN vnf_cnf ON cnf.id = vnf_cnf.cnf INNER JOIN vnf ON vnf.id = vnf_cnf.vnf WHERE cnf.live == 1 AND vnf.live = 0 ORDER BY cnf.nym;'
+sqlite3 dmnes.sqlite 'SELECT DISTINCT cnf.nym FROM cnf INNER JOIN vnf_cnf ON cnf.id = vnf_cnf.cnf INNER JOIN vnf ON vnf.id = vnf_cnf.vnf WHERE cnf.live == 1 AND vnf.live == 0 ORDER BY cnf.nym;'
