70968ccc8f29303bb10767277a642a4cbae5b486
Joel Uckelman <uckelman@nomic.net>
2015-10-04 22:55:46 +0100

    Fix the front page URL in index.html.

--- scripts/add_archival_note.sh
+++ scripts/add_archival_note.sh
@@ -2,3 +2,5 @@
 
 cd $1
 find -type f -printf '%P\0' | xargs -0 -n 1 -I '{}' sed -i '/<header>/a \ \ \ \ <div id="archival_note">This page is historical. Follow <a href="http://dmnes.org/{}">this link</a> for the current version.<\/a><\/div>' "{}"
+
+sed -i '/archival_note/s/http:\/\/dmnes.org\/index.html/http:\/\/dmnes.org\//' index.html
