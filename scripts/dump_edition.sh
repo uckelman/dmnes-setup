1bcc750cd7a88e612ae50a52964e9ce9693efe03
Joel Uckelman <uckelman@nomic.net>
2016-07-17 23:27:08 +0100

    Fixed extensions of .eot files when dumping edition.

--- scripts/dump_edition.sh
+++ scripts/dump_edition.sh
@@ -5,3 +5,6 @@ NUMBER=$2
 DST=$3
 
 wget -r -k -l0 -nH --restrict-file-names=nocontrol -P$DST/$YEAR/$NUMBER 'http://127.0.0.1:5000/names'
+
+# fix exentsions of .eot fonts
+for i in $(find $DST/$YEAR/$NUMBER -name *.eot?) ; do mv $i $(echo $i | sed 's/?$//') ; done
