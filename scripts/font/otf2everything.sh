6bc3ccb7b2e0c8d2fe6c6fef1f9d17ca008d5e8a
Joel Uckelman <uckelman@nomic.net>
2017-08-27 22:03:19 +0100

    Use python3.

--- scripts/font/otf2everything.sh
+++ scripts/font/otf2everything.sh
@@ -3,7 +3,7 @@
 for otf in "$@"; do
   base=$(basename -s .otf "$otf")
   sfnt2woff "$otf"
-  python -c "import fontforge
+  python3 -c "import fontforge
 fontforge.open('$otf').generate('${base}.ttf')"
   mkeot "${base}.ttf" >"${base}.eot"
   woff2/woff2_compress "${base}.ttf"
