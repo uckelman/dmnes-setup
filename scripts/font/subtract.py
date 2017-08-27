8dd67e7eec513b270bb8c1507f9bdb01d9028fc9
Joel Uckelman <uckelman@nomic.net>
2017-08-27 21:51:33 +0100

    Adjustments for Python 3 strings.

--- scripts/font/subtract.py
+++ scripts/font/subtract.py
@@ -3,9 +3,9 @@
 import fontforge
 import sys
 
-chars = set(c for c in sys.stdin.read().decode('utf-8').strip())
+chars = set(c for c in sys.stdin.read().strip())
 
 for f in sys.argv[1:]:
     chars -= set(unichr(c.unicode) for c in fontforge.open(f).glyphs() if c.unicode != -1)
 
-print(''.join(sorted(chars)).encode('utf-8'), end='')
+print(''.join(sorted(chars)), end='')
