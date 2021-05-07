d9cd56afa3d481a1a24d1e6c0831693292a84398
Joel Uckelman <uckelman@nomic.net>
2021-05-07 20:17:59 +0100

    Use HTTPS instead of HTTP.

--- scripts/add_archival_note.sh
+++ scripts/add_archival_note.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 cd $1
-find -type f -printf '%P\0' | xargs -0 -n 1 -I '{}' sed -i '/<header>/a \ \ \ \ <div id="archival_note">This page is historical. Follow <a href="http://dmnes.org/{}">this link</a> for the current version.<\/a><\/div>' "{}"
+find -type f -printf '%P\0' | xargs -0 -n 1 -I '{}' sed -i '/<header>/a \ \ \ \ <div id="archival_note">This page is historical. Follow <a href="https://dmnes.org/{}">this link</a> for the current version.<\/a><\/div>' "{}"
 
-sed -i '/archival_note/s/http:\/\/dmnes.org\/index.html/http:\/\/dmnes.org\//' index.html
+sed -i '/archival_note/s/https\?:\/\/dmnes.org\/index.html/https:\/\/dmnes.org\//' index.html
