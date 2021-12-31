3b3e53de9450059abb8009988e7a6718e5eadea9
Joel Uckelman <uckelman@nomic.net>
2021-12-31 16:15:12 +0000

    Don't use HTTPS for communicating with the local server.

--- scripts/dump_edition.sh
+++ scripts/dump_edition.sh
@@ -4,7 +4,7 @@ YEAR=$1
 NUMBER=$2
 DST=$3
 
-wget -r -k -l0 -nH --restrict-file-names=nocontrol -P$DST/$YEAR/$NUMBER 'https://127.0.0.1:5000/names'
+wget -r -k -l0 -nH --restrict-file-names=nocontrol -P$DST/$YEAR/$NUMBER 'http://127.0.0.1:5000/names'
 
 # fix exentsions of .eot fonts
 for i in $(find $DST/$YEAR/$NUMBER -name *.eot?) ; do mv $i $(echo $i | sed 's/?$//') ; done
